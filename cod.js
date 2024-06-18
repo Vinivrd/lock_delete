67a1e5e101888aeec21665f0a9b840ea51a1d867
viniciuscmbr <viniciuscmbrr@gmail.com>
2024-06-18 10:12:50 -0300

    Ok_add_displinas

--- cod.js
+++ cod.js
@@ -1,27 +1,15 @@
 //decarations
-var url = "https://docs.google.com/spreadsheets/d/12gRuEgIVOPZhCpM8qFeGmLmJd-aPeBBgxP6iNRwZr38/edit#gid=1304623703";
+const url = "https://docs.google.com/spreadsheets/d/1QaV4EX1lUkpWk-_P3fAjOsqvDpA_0lzdehBUnIgqxs0/edit#gid=0";
+const url_displinas = "https://docs.google.com/spreadsheets/d/1M2TFPWG1Viaw_IXDrpSFR7kXL9AWAisOl7tmLi6JHek/edit#gid=0";
 var Route = {};
 
-Route.path =  (route,callback) => {
-  Route[route] = callback;
-}
 
 function doGet(e){
-  Route.path("form",loadForm);
-
-  if(Route[e.parameters.v]) {
-    return Route[e.parameters.v]();
-  }else{
-    return HtmlService.createTemplateFromFile("html/home").evaluate();
-  }
-}
-
-function loadForm(){
-  
   let tmp = HtmlService.createTemplateFromFile("html/home");
-  tmp.title = "Curso Especial";
 
- 
+  tmp.title = "Curso Especial";
+  tmp.cursos = getCourses();
+  tmp.displinas = getDisplinas("Matemática- Núcleo Geral");
   return  tmp.evaluate();
 }
 
--- util/funcs.js
+++ util/funcs.js
@@ -1,5 +1,71 @@
-function userClicked(userInfo){
 
+function getCourses(){
+   let ss = SpreadsheetApp.openByUrl(url);
+   let ws = ss.getSheetByName("Cursos");
+   let list = ws.getRange(1,1,ws.getRange("A1").getDataRegion().getLastRow(),1).getValues();
+   return list.map(value => value[0]);
+}
+
+function getDisplinas (curso){
+  let curso_name;
+  let lista;
+  
+  switch (curso) {
+    case "Bacharelado em Ciência de Dados":
+      curso_name = "BCD";
+      lista = get_displinas_date(curso_name);
+      break;
+    case "Bacharelado em Matemática":
+      curso_name = "BMA";
+      lista = get_displinas_date(curso_name);
+      break;
+    case "Matemática- Núcleo Geral":
+      curso_name = "MATNG";
+      lista = get_displinas_date(curso_name);
+      break;
+    case "Licenciatura em Matemática":
+      curso_name = "LMA";
+      lista = get_displinas_date(curso_name);
+      break;
+    case "Bacharelado em Estatística e Ciência de Dados":
+      curso_name = "BECD";
+      lista = get_displinas_date(curso_name);
+      break;
+    case "Bacharelado em Sistema de Informação":
+      curso_name = "BSI";
+      lista = get_displinas_date(curso_name);
+      break;
+    case "Bacharelado em Ciências de Computação":
+      curso_name = "BCC";
+      lista = get_displinas_date(curso_name);
+      break;
+    case "Bacharelado em Matemática Aplicada e Computação Científica":
+      curso_name = "BMACC";
+      lista = get_displinas_date(curso_name);
+      break;
+    default:
+      break;
+  }
+  return lista;
+}
+
+function get_displinas_date(curso){
+  let ss = SpreadsheetApp.openByUrl(url_displinas);
+  let ws = ss.getSheetByName(curso);
+  let range = ws.getRange(1, 1, ws.getLastRow(), ws.getLastColumn()).getValues();
+
+  let result = range.map(row => {
+    return [row[1], row[2], row[3]]; // Corrigindo a forma como o array é criado
+  });
+  
+  // Remove o cabeçalho se existir
+  result.shift();
+  return result;
+}
+
+
+
+function userClicked(userInfo){
     var ss = SpreadsheetApp.openByUrl(url);
     var ws = ss.getSheetByName("Data");
    
