8957912bdf273fb2f3100cace067a1c327f33ad7
viniciuscmbr <viniciuscmbrr@gmail.com>
2024-06-21 08:34:50 -0300

    Consegui arrumar a validação dos créditos

--- cod.js
+++ cod.js
@@ -1,5 +1,6 @@
 //decarations
 const url = "https://docs.google.com/spreadsheets/d/1QaV4EX1lUkpWk-_P3fAjOsqvDpA_0lzdehBUnIgqxs0/edit#gid=0";
+const urlSubmit = "https://docs.google.com/spreadsheets/d/1XX5M_vGdwalfXieTrjk2_oYQPzHz9pUmMmD6U0KVSYQ/edit#gid=0"
 const url_displinas = "https://docs.google.com/spreadsheets/d/1M2TFPWG1Viaw_IXDrpSFR7kXL9AWAisOl7tmLi6JHek/edit#gid=0";
 var Route = {};
 
--- util/funcs.js
+++ util/funcs.js
@@ -66,20 +66,8 @@ function get_displinas_date(curso){
 
 
 function userClicked(userInfo){
-    var ss = SpreadsheetApp.openByUrl(url);
-    var ws = ss.getSheetByName("Data");
-   
-    //coloco todos dados do usúario na planilha 
-    ws.appendRow(
-      [
-        userInfo.firstName.toUpperCase(),
-        userInfo.lastName.toUpperCase(),
-        userInfo.selectedApp,
-        userInfo.email,
-        userInfo.num_usp,
-        userInfo.dep,
-        new Date()
-      ]
-    );
-    //Logger.log(name + " Clicked the button");
+    var ss = SpreadsheetApp.openByUrl(urlSubmit);
+    var ws = ss.getSheetByName("userInfos");
+    
+    ws.appendRow(userInfo);
 }
