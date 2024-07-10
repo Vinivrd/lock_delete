2deec01622b4d6d197c557f8f32be8b9bb2ef3b7
viniciuscmbr <viniciuscmbrr@gmail.com>
2024-07-10 09:13:41 -0300

    first

--- util/funcs.js
+++ util/funcs.js
@@ -96,7 +96,7 @@ function takeIntervalDate() {
   return interval;
 }
 
-function uploadFile(file,file2,nome) {
+function upload File(file,file2,nome) {
   try {
     var mainFolder  = DriveApp.getFolderById('1KBRAw4hk1rJGD8g6NGQNOwZcbceflYKX');
 
